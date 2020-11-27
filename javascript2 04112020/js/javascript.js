a27e4136060b4cb0783c386f3598389fccc184ab
João Vitor Gomes Pontes <joao.pontes29@etec.sp.gov.br>
2020-11-27 06:53:24 -0300

    Update
    27_11_2020

---
+++ javascript2 04112020/js/javascript.js
@@ -0,0 +1,24 @@
+function corum(){
+    var azul=document.getElementById("texto")
+    azul.style.color="#0000ff";
+}
+function cordois(){
+    var verde=document.getElementById("texto")
+    verde.style.color="#008000";
+}
+function cortres(){
+    var marrom=document.getElementById("texto")
+    marrom.style.color="#8b4513";
+}
+function corquatro(){
+    var azulc=document.getElementById("fundo")
+    azulc.style.backgroundColor="#add8e6";
+}
+function corcinco(){
+    var cinza=document.getElementById("fundo")
+    cinza.style.backgroundColor="#4f4f4f";
+}
+function corseis(){
+    var amarelo=document.getElementById("fundo")
+    amarelo.style.backgroundColor="#ffff00";
+}
